97af083c1ba816fcbb8a2916861bd14a005c71db
Azure-317 <151602308+Azure-317@users.noreply.github.com>
2024-03-15 00:03:25 -0500

    declared id Variables in js

---
+++ assets/script.js
@@ -0,0 +1,6 @@
+var ViewHighScoreEl = document.getElementById("view-score");
+var MainContainerel = document.getElementById("Main");
+var StartButtonEl = document.getElementById("start-button");
+var QuestionContainerEl = document.getElementById("ques-cont");
+var QuestionEl = document.getElementById("question");
+var AnswerButEl = document.getElementById("answer");
